a54e9ea0448c41cae6fae9ca2f9ab6fb4e9e6abc
Kushagra Johari <skjkushagra1309@gmail.com>
2021-07-16 22:05:29 +0530

    more metas

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ async function Crawler(url) {
         }, (err, res, body) => {
             //console.log(res);
             var $ = cheerio.load(res.body)
+            var pageUrl = $('head > meta[property="og:url"]').attr('content');
+            var type = $('head > meta[property="og:type"]').attr('content');
             var image = $('meta[property="og:image"]').attr('content');
+            var imageWidth = $('meta[property="og:image:width"]').attr('content');
+            var imageHeight = $('meta[property="og:image:height"]').attr('content');
             var description = $('meta[property="og:description"]').attr('content');
+            var heading = $('meta[property="og:title"]').attr('content');
+            var content = $('meta[name="description"]').attr('content');
             var time = $('meta[property="article:published"]').attr('content');
+            var keywords = $('meta[name="keywords"]').attr('content');
+        
             var title = {
+                pageUrl,
+                heading,
+                type,
                 image: image,
+                imageWidth,
+                imageHeight,
                 description: description,
+                content,
+                keywords,
                 time: time
             };
+           
             resolve(title);
 
         });
