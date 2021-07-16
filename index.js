a07ec30198bde421c63abb66080b435bbe3a0c66
Shaharyar Shamshi <shaharyarshamshi@gmail.com>
2021-07-16 13:37:38 +0530

    scrapper added

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-exports.printMsg = ()=>{
-    console.log('*************************Welcome Team! Love to have you onboard!!!!**********************');
+const cheerio = require("cheerio");
+const request = require("request");
+exports.Crawler = Crawler;
+async function Crawler(url) {
+    return new Promise((resolve, reject) => {
+        request({
+            method: 'GET',
+            url: url
+        }, (err, res, body) => {
+            //console.log(res);
+            var $ = cheerio.load(res.body)
+            var image = $('meta[property="og:image"]').attr('content');
+            var description = $('meta[property="og:description"]').attr('content');
+            var time = $('meta[property="article:published"]').attr('content');
+            var title = {
+                image: image,
+                description: description,
+                time: time
+            };
+            resolve(title);
+
+        });
+    })
 }
