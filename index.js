987f392a5fa982581a6581f53275f1750fa552d7
Shaharyar Shamshi <shaharyarshamshi@gmail.com>
2023-01-04 16:18:11 +0530

    migrated to playwright

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
-const cheerio = require("cheerio");
-const needle = require("needle");
+const playwright = require('playwright');
 exports.Crawler = Crawler;
 async function Crawler(url) {
-    return new Promise((resolve, reject) => {
-        needle({
-            method: 'GET',
-            url: url
-        }, (err, res, body) => {
-            //console.log(res);
-            var $ = cheerio.load(res.body)
-            var pageUrl = $('head > meta[property="og:url"]').attr('content');
-            var type = $('head > meta[property="og:type"]').attr('content');
-            var image = $('meta[property="og:image"]').attr('content');
-            var imageWidth = $('meta[property="og:image:width"]').attr('content');
-            var imageHeight = $('meta[property="og:image:height"]').attr('content');
-            var description = $('meta[property="og:description"]').attr('content');
-            var heading = $('meta[property="og:title"]').attr('content');
-            var content = $('meta[name="description"]').attr('content');
-            var time = $('meta[property="article:published"]').attr('content');
-            var keywords = $('meta[name="keywords"]').attr('content');
-        
-            var title = {
-                pageUrl,
-                heading,
-                type,
-                image: image,
-                imageWidth,
-                imageHeight,
-                description: description,
-                content,
-                keywords,
-                time: time
-            };
-           
-            resolve(title);
+  const metadata = await main(url);
+  return metadata;
+  
+}
 
-        });
-    })
+async function main(url) {
+  const browser = await playwright.chromium.launch({
+      headless: true // setting this to true will not run the UI
+  });
+  
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitForTimeout(5000); // wait for 5 seconds
+  const metadata = await page.evaluate(() => {
+    return {
+      heading: document.title,
+      description: document.querySelector('meta[property="og:description"]').content,
+      image: document.querySelector('meta[property="og:image"]').content
+    };
+  });
+  
+  
+  await browser.close();
+  return metadata;
 }
